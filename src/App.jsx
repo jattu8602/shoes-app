ef7787d4e1fe4add495232c3b992e016a30b28f9
Jatin <chaurasiyajatin68@gmail.com>
2024-06-19 02:21:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+const App = () =>{
+  return <div>
+    <Navigation/>
+    <Hero/>
+
+
+  </div>;
+
+
+
+
+
+
+
+
+
+
+
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+const Navigation = () =>{
+  return  (<nav className='container'> 
+  <div className="logo">
+   <img src="/images/brand_logo.png" alt="logo" />
+  </div>
+
+  <ul>
+    <li href="#">Menu</li>
+    <li href="#">Location</li>
+    <li href="#">About Us</li>
+    <li href="#">Contact</li>
+  </ul>
+
+  <button>Login</button>
+
+
+
+
+
+
+
+
+
+
+</nav>);
+
+
+};
+export default Navigation;
